461cf550e5a41a9ca2baa0d40e6849d0f4c38a4a
Ely Alamillo <ely0013@gmail.com>
2019-04-13 15:16:34 -0500

    add initial config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+
+import Navigation from './components/navigation/navigation';
+// import Footer from './components/footer/footer';
+// import Home from './components/home/home';
+// import Jobs from './components/jobs/jobs';
 import './App.css';
 
 class App extends Component {
-  render() {
+   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Navigation />
+        {/* <Route path="/" exact={true} component={Home} /> */}
+        {/* <Route path="/jobs" exact={true} component={Jobs} /> */}
+        {/* <Footer /> */}
       </div>
     );
   }
---
+++ src/components/navigation/navigation.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { Component } from 'react';
+
+import './navigation.css';
+
+class Navigation extends Component {
+   navToggle = () => {
+    // ! is a typescript hack, need to revist implementation
+    document.querySelector('.navbar-menu').classList.toggle('is-active');
+  };
+
+   render() {
+    return (
+      <nav
+        className="navbar has-background-info"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <div className="navbar-item">
+              <Link to="/">
+                {/* <img
+                  src="https://bulma.io/images/bulma-logo.png"
+                  alt="Bulma: a modern CSS framework based on Flexbox"
+                  width="112"
+                  height="28"
+                /> */}
+                <h1 className="title has-text-white">On The Bloc.</h1>
+              </Link>
+            </div>
+            <a
+              role="button"
+              className="navbar-burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navMenu"
+              onClick={this.navToggle}
+              href="#nope"
+            >
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+            </a>
+          </div>
+
+          <div className="navbar-menu" id="navMenu">
+            <div className="navbar-end">
+              <div className="navbar-item">
+                <NavLink
+                  to="/"
+                  className="is-medium is-link"
+                  activeClassName="is-active"
+                  exact={true}
+                >
+                  <p className="has-text-white">Home</p>
+                </NavLink>
+              </div>
+              <div className="navbar-item">
+                <NavLink
+                  to="/jobs"
+                  className="is-medium is-link"
+                  activeClassName="is-active"
+                  exact={true}
+                >
+                  <p className="has-text-white">Jobs</p>
+                </NavLink>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navigation;
