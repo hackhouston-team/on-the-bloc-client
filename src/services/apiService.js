5088305db6a69ca7c8be5fd3f8e1ee4cb7788eb2
Ely Alamillo <ely0013@gmail.com>
2019-04-14 01:07:14 -0500

    add apiService

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,23 +1,45 @@
+import axios from 'axios';
+
 class ApiService {
-  baseUrl = `https://whatever.com`
-  blocsUrl = `${this.baseUrl}/blocs/`
-  usersUrl = `${this.baseUrl}/users/`
+  baseUrl = `https://whatever.com/api/v1`;
+  usersUrl = `${this.baseUrl}/users/`;
+  blocsUrl = `${this.baseUrl}/blocs/`;
 
+  // user endpoints
+  getUser = async id => {
+    const { data } = await axios.get(`${this.usersUrl}/${id}`);
+    return data;
+  };
 
-  getBlock = () => { }
+  getUserMessages = () => {}; // low priority
 
-  getUser = () => { }
+  createUser = async username => {
+    const { data } = axios.post(`${this.usersUrl}`, { id: username });
+    return data;
+  };
 
-  getUserMessages = () => { }
+  // bloc endopoints
+  getBloc = async id => {
+    const { data } = await axios.get(`${this.blocsUrl}/${id}`);
+    return data;
+  };
 
-  postBloc = () => { }
+  // creates a new bloc
+  postBloc = async (creatorId, blocName) => {
+    const { data } = await axios.post(`${this.blocsUrl}`, {
+      creatorId,
+      name: blocName
+    });
+    return data;
+  };
 
-  postBloc = () => { }
-
-  postBlocMessage = () => { }
-
-  createUser = () => { }
+  postBlocMessage = async (creatorId, blocId, content) => {
+    const { data } = await axios.post(`${this.blocsUrl}/${blocId}/message`, {
+      creatorId,
+      content
+    });
+    return data;
+  };
 }
 
-
-export default ApiService
+export default new ApiService();
