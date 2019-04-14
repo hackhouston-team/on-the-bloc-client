aead5cd52204e1167416429afb85ab54ed273769
Ely Alamillo <ely0013@gmail.com>
2019-04-14 10:14:50 -0500

    center style button

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -3,16 +3,22 @@ import React from 'react'
 
 const Login = ({ isSignedIn, handleSignIn, handleSignOut }) => {
   return (
-    <div>
+    <div className="has-text-centered">
       <p style={{ display: isSignedIn ? 'none' : 'block' }}>
-        <button onClick={handleSignIn}>
+        <button
+          className="button button-primary"
+          onClick={handleSignIn}
+        >
           Sign-in with Blockstack
           </button>
       </p>
       <p style={{ display: !isSignedIn ? 'none' : 'block' }}>
-        <button onClick={handleSignOut}>
+        <button
+          className="button button-primary"
+          onClick={handleSignOut}
+        >
           Sign-out
-          </button>
+        </button>
       </p>
     </div>
   )
